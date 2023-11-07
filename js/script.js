1bbbc83c3043769f090e96e6e7f6cb8f0b8eca7b
LeonidMarushkin <139209971+LeonidMarushkin@users.noreply.github.com>
2023-11-07 15:45:39 +0300

    homework 17+18

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 console.log('Hello World!');
 let name = prompt('Enter Name');
 alert(`Hello ${name}`);
+
+let a = +prompt('Введите первое число');
+let b = +prompt('Введите второе число');
+
+let min = a;
+
+if (min === b) {
+    console.log(`Числа равны`);    
+} else {
+    if (min > b) {
+        console.log(`Минимальное число ${b}`);  
+    } else {
+        console.log(`Минимальное число ${a}`);    
+    }        
+}
